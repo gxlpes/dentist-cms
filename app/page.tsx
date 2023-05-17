321e8b296c34bc63e210e6ffe0dc7213ee5f0e9d
Guilherme Lopes <guilhermxlopes@gmail.com>
2023-05-16 21:52:58 -0300

    initial project

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className="bg-slate-500">
+      <h1 className='text-4xl'>
+        Welcome to the blog
+      </h1></div>
+  )
+}
+
+export default page
